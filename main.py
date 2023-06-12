4d46a29bccecd4a84a6c27d652897a3bdace22bf
gugul <133491980+gugul@users.noreply.github.com>
2023-06-12 07:27:44 +0200

    Create columns, image and description

---
+++ main.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.jpg")
+
+with col2:
+    st.title("Adam Pastuszka")
+    content = """Info about me
+    
+    """
+    st.info(content)
